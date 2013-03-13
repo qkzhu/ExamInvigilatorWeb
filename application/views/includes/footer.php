8f17fcef1bbc0d725743f2d20b1b58dc17048f34
Qian Kun <zqkun520@gmail.com>
2013-03-13 14:57:28 +0800

    added .gitignore and made changes for footer css file

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,7 +1,10 @@
-<strong>
-	<ul>
-		<li>&copy; 2013 Exam Invigilator Web Tool</li>
-		<li><a href="#">Terms of Services</a></li>
-		<li><a href="#">Privacy Police</a></li>
-	</ul>
-</strong>
+<div id="footer_wraper">
+	<div id="footer">
+		<strong>
+			<ul>
+				<li>Copyright &copy; 2013 Exam Invigilator Web Tool</li>
+				<li>Design &amp; Implemented by Xiang Ran</li>
+			</ul>
+		</strong>
+	</div>
+</div>
